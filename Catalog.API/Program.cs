dd6050746569b2dd1c3a21dbd19da556b292b407
Miguel Brito <miguelfelipe_steam@outlook.com>
2024-06-01 16:11:25 -0300

    feat: adding support for multiple enviroment appsettings.json

--- Catalog.API/Program.cs
+++ Catalog.API/Program.cs
@@ -1,4 +1,16 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure App to use multiple configuration files
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+
 
 // Add services to the container.
 
@@ -6,6 +18,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICatalogContextSeed, CatalogContextSeed>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
