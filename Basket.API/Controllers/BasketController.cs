c11a6b938ad5d25c142ecff4082fd7f58bc73de9
Miguel Brito <miguelfelipe_steam@outlook.com>
2024-06-03 20:46:38 -0300

    feat: adding basket api crud using repository pattern and redis

---
+++ Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;            
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _basketRepository.GetBasketAsync(userName);
+
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket(ShoppingCart basket)
+        {
+            return Ok(await _basketRepository.UpdateBasketAsync(basket)); 
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        public async Task<ActionResult<ShoppingCart>> DeleteBasket(string userName)
+        {
+            await _basketRepository.DeleteBasketAsync(userName);
+            return Ok();
+        }
+    }
+
+}
