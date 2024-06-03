19cf1e2e963d248ee4baac31f6c7c0575f27479c
Miguel Brito <miguelfelipe_steam@outlook.com>
2024-06-03 02:29:34 -0300

    feat: starting basket api structure

---
+++ Basket.API/Enums/PaymentMethod.cs
@@ -0,0 +1,15 @@
+﻿namespace Basket.API.Enums
+{
+    public enum PaymentMethod
+    {
+        CreditCard = 0,
+        DebitCard = 1,
+        PayPal = 2,
+        BankTransfer = 3,
+        Cash = 4,
+        GiftCard = 5,
+        Cryptocurrency = 6,
+        ApplePay = 7,
+        GooglePay = 8
+    }
+}
