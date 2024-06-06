bf7a3bf7a9a4650ae222c0ab9204c724aa4a195c
Miguel Brito <miguelfelipe_steam@outlook.com>
2024-06-06 03:45:48 -0300

    feat: adding discount microservice using gRPC approach

---
+++ Discount.Grpc/Repositories/DiscountRepository.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using Discount.Grpc.Entities;
+using Npgsql;
+
+namespace Discount.Grpc.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private NpgsqlConnection GetConnectionPostgreSQL()
+        {
+            return new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+
+        public async Task<Coupon?> GetDiscountAsync(string productName)
+        {
+            var connection = GetConnectionPostgreSQL();
+
+            return await connection
+                .QueryFirstOrDefaultAsync<Coupon>("SELECT * FROM Coupon WHERE ProductName = @ProductName"
+                , new { ProductName = productName });
+        }
+
+        public async Task<bool> CreateDiscountAsync(Coupon coupon)
+        {
+            var connection = GetConnectionPostgreSQL();
+
+            var affected = await connection.ExecuteAsync("INSERT INTO Coupon (ProductName, Description, Amount)" +
+                " VALUES (@ProductName, @Description, @Amount)",
+                new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+
+            if (affected == 0) return false;
+
+            return true;
+        }
+        public async Task<bool> UpdateDiscountAsync(Coupon coupon)
+        {
+            var connection = GetConnectionPostgreSQL();
+
+            var affected = await connection
+                .ExecuteAsync("UPDATE coupon SET productname = @ProductName, description = @Description, amount = @Amount WHERE id = @Id",
+                new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount, Id = coupon.Id });
+
+            if (affected == 0) return false;
+
+            return true;
+        }
+
+        public async Task<bool> DeleteDiscountAsync(string id)
+        {
+            var connection = GetConnectionPostgreSQL();
+
+            var affected = await connection
+                .ExecuteAsync("DELETE FROM Coupon WHERE Id = @Id",
+                new { Id = id });
+
+            if (affected == 0) return false;
+
+            return true;
+        }
+    }
+}
---
+++ Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _discountRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository discountRepository, IMapper mapper, ILogger<DiscountService> logger)
+        {
+            _discountRepository = discountRepository;
+            _mapper = mapper;   
+            _logger = logger;
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _discountRepository.GetDiscountAsync(request.Productname);
+            if (coupon == null)
+                throw new RpcException(new Status(StatusCode.NotFound,
+                    $"Discount for product = {request.Productname} not found."));
+
+            _logger.LogInformation($"Discount retrieved for product {coupon.ProductName}");
+
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+
+            return couponModel;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = _mapper.Map<Coupon>(request.Coupon);
+            bool created = await _discountRepository.CreateDiscountAsync(coupon);
+            if (!created)
+                throw new RpcException(new Status(StatusCode.InvalidArgument,
+                    $"An error has occurred while trying to create the product {request.Coupon.Productname}"));
+
+            return _mapper.Map<CouponModel>(coupon);
+        }
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = _mapper.Map<Coupon>(request.Coupon);
+            bool updated = await _discountRepository.UpdateDiscountAsync(coupon);
+            if (!updated)
+                throw new RpcException(new Status(StatusCode.InvalidArgument,
+                    $"An error has occurred while trying to update the product {request.Coupon.Productname}"));
+
+            return _mapper.Map<CouponModel>(coupon);
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            if (!await _discountRepository.DeleteDiscountAsync(request.Productname))
+                throw new RpcException(new Status(StatusCode.InvalidArgument,
+                    $"An error has occurred while trying to delete the product {request.Productname}"));
+
+            return new DeleteDiscountResponse { Success = true };
+        }
+    }
+}
